e8b4f16228c571cd5c1bd50c73a602f6266f86e7
Adam Sequeira <adam.sequeira@sportsiq.ai>
2023-12-02 00:55:53 -0500

    new commit message

--- dags/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="dags",
-    packages=find_packages(exclude=["dags_tests"]),
-    install_requires=["dagster", "pandas", "dagster-cloud"],
-    extras_require={"dev": ["dagster-webserver", "pytest"]},
-)
